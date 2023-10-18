9626c0e44623cf15adafc6df791958fd3a1973f0
Niil <91337648+neeeal@users.noreply.github.com>
2023-10-18 12:54:22 +0800

    added new time model

--- pages/1_Classification_Page.py
+++ pages/1_Classification_Page.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Classification Page", page_icon="🔎")
 
 @st.cache_data
 def load_model():
-    model = tf.keras.models.load_model("assets/time_model.h5", compile=False)
+    model = tf.keras.models.load_model("assets/model.h5", compile=False)
     model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['acc'])
     return model
 

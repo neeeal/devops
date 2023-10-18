a2c78b5e14cf94b2c4eea7c092b0c12f347971de
Niil <91337648+neeeal@users.noreply.github.com>
2023-10-18 12:32:47 +0800

    updated test_sample using functions from classification page

--- test_sample.py
+++ test_sample.py
@@ -4,19 +4,17 @@ import numpy as np
 from PIL import Image,ImageOps
 
 def load_model():
-    model = tf.keras.models.load_model("assets/best_model.h5",compile=False)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer="adam",
-                  metrics=['acc'])
+    model = tf.keras.models.load_model("assets/best_model.h5", compile=False)
+    model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['acc'])
     return model
 
-def import_and_predict(image_data,model):
-    size =  (224,224)
-    image = ImageOps.fit(image_data,size,Image.LANCZOS)
-    img=np.asarray(image)
+def import_and_predict(image_data, model):
+    size = (224,224)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    img = np.asarray(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_reshape=gray[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+    img_reshape = gray[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
 
 def test_prediction():
